9a17471adcb39e1e4d5eca673d07b1e5dad2e75a
Jamie Barton <jbarton10@email.arizona.edu>
2019-06-03 16:44:47 -0700

    Things are working better, need to be able to add classes to divs created in js tho

--- assets/javascrpit/app.js
+++ assets/javascrpit/app.js
@@ -13,6 +13,8 @@ document.getElementById("startDiv").addEventListener("click",question1);
 
 
 function timer(){
+    console.log("we here");
+    document.getElementById("startDiv").outerHTML = "";
     //Maybe redifine questionTime in here so this can be reused... Or maybe only define question time in here
     clearInterval(intervalID);
     intervalID = setInterval(decrement,1000);
@@ -21,6 +23,7 @@ function decrement(){
     questionTime--;
 
     //Need to display timer to a div/span here
+    //document.getElementById("startDiv").textContent= questionTime;
     if(questionTime===0){
         loss();
     }
@@ -28,10 +31,20 @@ function decrement(){
 }
 function loss(){
 
+
+}
+function win(){
+
+
 }
 
 function question1(){
-    var question1="What is the name of the programmer?"
+    timer();
+    var currentQuestion= document.createElement("div");
+    currentQuestion.textContent = "What is the name of the programmer?"
+    currentQuestion.setAttribute("class", "currentQuestion");
+    document.getElementById("gameContent").appendChild(currentQuestion);
+    
     correctAnswer="Jamie Barton";
     incorrect1="Jeff";
     incorrect2="Eric";
